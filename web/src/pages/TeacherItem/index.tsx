dd9acd2aaf25a2193227f91ea08c112daa17c6e0
Marcelo Correa <marcelocorreaads@gmail.com>
2020-08-14 18:05:30 -0300

    finalização de layout e conexao com back end

--- web/src/pages/TeacherItem/index.tsx
+++ web/src/pages/TeacherItem/index.tsx
@@ -1,40 +1,51 @@
 import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 import './styles.css';
 
-function TeacherItem (){
+export interface Teacher{
+    id: number;
+    subject: string;
+    cost: number;
+    name: string;
+    avatar: string;
+    whatsapp: string;
+    bio: string;
+}
+
+interface TeacherItemProps{
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection(){
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
         <header>
-            <img src="https://avatars1.githubusercontent.com/u/59809579?s=460&u=1c96cff34ecc10b16aef63b1f0ccd706b53c1106&v=4" alt="Marcelo Corrêa"/>
+            <img src={teacher.avatar} alt={teacher.name} />
             <div>
-                <strong>Marcelo Corrêa</strong>
+                <strong>{teacher.name}</strong>
                 <span>
-                    Química
+                    {teacher.subject}
                 </span>
             </div>
         </header>
-        <p>
-            Entusiata das melhores tecnologias de quimica avançada.
-            <br/><br/>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc in ex vitae felis faucibus laoreet. 
-            Sed nec nulla ut ex vestibulum tempor. In hac habitasse platea dictumst. 
-            Aliquam erat volutpat. Aenean non pulvinar diam. Duis eu tincidunt elit, et gravida nisl. 
-            Suspendisse imperdiet ullamcorper mi, sed aliquam nibh congue non. Praesent porttitor accumsan vestibulum. 
-            Pellentesque turpis tellus, mattis eu ante nec, placerat vestibulum massa. 
-            Etiam ante erat, rhoncus eget eros sit amet, rhoncus fringilla magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Nullam at nulla mattis, lobortis felis ut, posuere mauris.
-        </p>
+        <p>{teacher.bio}</p>
         <footer>
             <p>
                 Preço/hora
-                <strong>R$ 80,00</strong>
+                <strong>R$ {teacher.cost} </strong>
             </p>
-            <button type="button">
-                    <img src={whatsappIcon} alt="Whatsapp"/>
-                    Entrar em contato
-                </button>
+            <a onClick={ createNewConnection } href={`https://wa.me/${teacher.whatsapp}`} target="_blank">
+                <img src={whatsappIcon} alt="Whatsapp"/>
+                Entrar em contato
+            </a>
         </footer>
     </article>
     )
