b6450480e0779f10e0558b1cdaf80c8d6083714d
Marcelo Correa <marcelocorreaads@gmail.com>
2020-10-01 17:15:30 -0300

    finalizada pagina GiveClasses

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Image, Text, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import styles from './styles';
 
@@ -24,19 +25,19 @@ function Landing(){
                 <Text style={styles.titleBold}>O que deseja fazer?</Text>
             </Text>
             <View style={styles.buttonsContainer}>
-                <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+                <RectButton style={[styles.button, styles.buttonPrimary]}>
                     <Image source={studyIcon} />
 
                     <Text style={styles.buttonText}>Estudar</Text>
-                </TouchableOpacity>
-                <TouchableOpacity 
+                </RectButton>
+                <RectButton 
                     onPress={handleNavigateToGiveClassesPage} 
                     style={[styles.button, styles.buttonSecondary]}
                 >
                     <Image source={giveClassesIcon} />
 
                     <Text style={styles.buttonText}>Dar Aula</Text>
-                </TouchableOpacity>
+                </RectButton>
             </View>
 
             <Text style={styles.totalConnections}>
